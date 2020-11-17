fd3cc8adfd523ceb394a191cbf34ab527b9014f1
skaadin <aritroshome.development@gmail.com>
2020-11-17 12:08:43 +0530

    Input from STDIN illustrated in input.go file.

---
+++ input.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func input() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the name")
+	var givenName, _ = reader.ReadString('\n')
+	fmt.Println("Your name is probabaly " + givenName + "but mine is Skaadin")
+}
